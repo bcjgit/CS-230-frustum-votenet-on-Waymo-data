d22649669ccebb0244684bbf08392743b5f6bcb2
Brian Johnson <bcj26@case.edu>
2020-11-17 09:13:57 -0800

    Brianj fix waymo dataset loading (#5)
    * add more waymo data loading code
    
    * data loading might work
    
    * progress
    
    * new loading maybe works
    
    * new dataset
    
    * fixed point net data hopefully
    
    * submit

---
+++ waymo/model_util_waymo.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+
+class WaymoDatasetConfig(object):
+
+    def __init__(self):
+        self.num_class = 4
+        self.num_heading_bin = 12 # Matching num_heading_bin in SUNRGDB dataset config
+        self.num_size_cluster = self.num_class # don't do additional clustering within a given class
+
+        self.type2class = {
+            'TYPE_SIGN': 2,
+            'TYPE_VEHICLE': 0,
+            'TYPE_PEDESTRIAN': 1,
+            'TYPE_CYCLIST': 3
+        }
+
+        self.class2type = {self.type2class[t]: t for t in self.type2class}
+        self.type2onehotclass= self.type2class # TODO this is in the original code ... not clear if it's a bug
+
+
+        self.type_mean_size = {
+                'TYPE_VEHICLE': np.array([1.94191027, 4.41998532, 1.97638222]), # CAR
+                'TYPE_PEDESTRIAN': np.array([0.8433135, 0.90025193, 1.71406664]), # PED
+                'TYPE_SIGN': np.array([0.63715241, 0.45786768, 0.76657946]), # SIGN
+                'TYPE_CYCLIST': np.array([0.9088676,  1.94448067, 1.78]), # CYCLIST
+            }
+
+        self.mean_size_arr = np.zeros((self.num_size_cluster, 3))
+        for i in range(self.num_size_cluster):
+            self.mean_size_arr[i, :] = self.type_mean_size[self.class2type[i]]
+
+    def size2class(self, size, type_name):
+        ''' Convert 3D box size (l,w,h) to size class and size residual '''
+        size_class = self.type2class[type_name]
+        size_residual = size - self.type_mean_size[type_name]
+        return size_class, size_residual
+
+    def class2size(self, pred_cls, residual):
+        ''' Inverse function to size2class '''
+        mean_size = self.type_mean_size[self.class2type[pred_cls]]
+        return mean_size + residual
+
+    def angle2class(self, angle):
+        ''' Convert continuous angle to discrete class
+            [optinal] also small regression number from
+            class center angle to current angle.
+
+            angle is from 0-2pi (or -pi~pi), class center at 0, 1*(2pi/N), 2*(2pi/N) ...  (N-1)*(2pi/N)
+            return is class of int32 of 0,1,...,N-1 and a number such that
+                class*(2pi/N) + number = angle
+        '''
+        num_class = self.num_heading_bin
+        angle = angle % (2 * np.pi)
+        assert (angle >= 0 and angle <= 2 * np.pi)
+        angle_per_class = 2 * np.pi / float(num_class)
+        shifted_angle = (angle + angle_per_class / 2) % (2 * np.pi)
+        class_id = int(shifted_angle / angle_per_class)
+        residual_angle = shifted_angle - (class_id * angle_per_class + angle_per_class / 2)
+        return class_id, residual_angle
+
+    def class2angle(self, pred_cls, residual, to_label_format=True):
+        ''' Inverse function to angle2class '''
+        num_class = self.num_heading_bin
+        angle_per_class = 2 * np.pi / float(num_class)
+        angle_center = pred_cls * angle_per_class
+        angle = angle_center + residual
+        if to_label_format and angle > np.pi:
+            angle = angle - 2 * np.pi
+        return angle
+
+    def param2obb(self, center, heading_class, heading_residual, size_class, size_residual):
+        heading_angle = self.class2angle(heading_class, heading_residual)
+        box_size = self.class2size(int(size_class), size_residual)
+        obb = np.zeros((7,))
+        obb[0:3] = center
+        obb[3:6] = box_size
+        obb[6] = heading_angle * -1
+        return obb
--- waymo/waymo_detection_dataset.py
+++ waymo/waymo_detection_dataset.py
@@ -1,31 +1,34 @@
-from torch.utils.data import Dataset
-import numpy as np
 import os
 import sys
+import numpy as np
+from torch.utils.data import Dataset
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+sys.path.append(ROOT_DIR)
+sys.path.append(os.path.join(ROOT_DIR, 'utils'))
 import pc_util
-# from model_util_waymo import WaymoDatasetConfig
-# DC = WaymoDatasetConfig()
-# TODO this is almost def wrong
-# from model_util_scannet import ScannetDatasetConfig
-# DC = ScannetDatasetConfig()
-MAX_NUM_OBJ = 64 # TODO probably too low
-MEAN_COLOR_RGB = np.array([109.8, 97.2, 83.8]) # TODO probably wrong
+#from model_util_waymo import rotate_aligned_boxes
+import sunrgbd_utils
+from model_util_waymo import WaymoDatasetConfig
 
+DC = WaymoDatasetConfig()
+MAX_NUM_OBJ = 256
+MEAN_COLOR_RGB = np.array(
+    [109.8, 97.2, 83.8])  # TODO probably wrong ... tbh it does not matter ... tbh it does not matter
 
+# docker run  --gpus all -it --ipc=host -v /home/ubuntu/data:/data -v /home/ubuntu/tf_logs:/work/tf_logs waymo_votenet /bin/bash
 class WaymoDetectionDataset(Dataset):
 
     def __init__(self, split_set='train', num_points=40000,
-                 use_color=False, use_height=False, augment=False, data_path = None):
+                 use_color=False, use_height=False, augment=False, data_path=None):
 
-        # TODO if you have loading errors probably look here first!
         if not data_path:
-            self.data_path = os.path.join('/work/data', 'waymo_train_dataset')
-        else:
-            self.data_path = data_path
+            raise ValueError('Please provide a data path')
+            # TODO implement logic for split sets other than train
+        self.scan_names = list(set([file.split('_')[0] for file in os.listdir(self.data_path) if 'npy' in file]))
 
-        # TODO implement logic for split sets other than train
-        self.scan_names = list(set([file.split('_')[0] for file in  os.listdir(self.data_path) if 'npy' in file]))
+        self.scan_names = list(set([file.split('_')[0] for file in os.listdir(self.data_path) if 'npy' in file]))
 
         self.num_points = num_points
         self.use_color = use_color
@@ -36,12 +39,11 @@ class WaymoDetectionDataset(Dataset):
         return len(self.scan_names)
 
     def __getitem__(self, idx):
-        print(self.scan_names)
         scan_name = self.scan_names[idx]
         mesh_vertices = np.load(os.path.join(self.data_path, scan_name) + '_vert.npy')
         instance_labels = np.load(os.path.join(self.data_path, scan_name) + '_ins_label.npy')
-        semantic_labels = np.load(os.path.join(self.data_path, scan_name) + '_sem_label.npy')
-        instance_bboxes = np.load(os.path.join(self.data_path, scan_name) + '_bbox.npy')
+        semantic_labels = np.load(os.path.join(self.data_path, scan_name) + '_sem_label.npy').astype(np.int32) - 1
+        bboxes = np.load(os.path.join(self.data_path, scan_name) + '_bbox.npy')
 
         if not self.use_color:
             point_cloud = mesh_vertices[:, 0:3]  # do not use color for now
@@ -65,31 +67,45 @@ class WaymoDetectionDataset(Dataset):
 
         point_cloud, choices = pc_util.random_sampling(point_cloud,
                                                        self.num_points, return_choices=True)
-        instance_labels = instance_labels[choices]
-        semantic_labels = semantic_labels[choices]
+        # instance_labels = instance_labels[choices]
+        # semantic_labels = semantic_labels[choices]
+        #
+        # pcl_color = pcl_color[choices]
 
-        pcl_color = pcl_color[choices]
-
-        target_bboxes_mask[0:instance_bboxes.shape[0]] = 1
-        target_bboxes[0:instance_bboxes.shape[0], :] = instance_bboxes[:, 0:6]
+        # target_bboxes_mask[0:instance_bboxes.shape[0]] = 1
+        # target_bboxes[0:instance_bboxes.shape[0], :] = instance_bboxes[:, 0:6]
 
         # ------------------------------- DATA AUGMENTATION ------------------------------
         if self.augment:
-            if np.random.random() > 0.5:
-                # Flipping along the YZ plane
-                point_cloud[:, 0] = -1 * point_cloud[:, 0]
-                target_bboxes[:, 0] = -1 * target_bboxes[:, 0]
+            pass
+            # if np.random.random() > 0.5:
+            #     # Flipping along the YZ plane
+            #     point_cloud[:, 0] = -1 * point_cloud[:, 0]
+            #     target_bboxes[:, 0] = -1 * target_bboxes[:, 0]
+            #
+            # if np.random.random() > 0.5:
+            #     # Flipping along the XZ plane
+            #     point_cloud[:, 1] = -1 * point_cloud[:, 1]
+            #     target_bboxes[:, 1] = -1 * target_bboxes[:, 1]
+            #
+            #     # Rotation along up-axis/Z-axis
+            # rot_angle = (np.random.random() * np.pi / 18) - np.pi / 36  # -5 ~ +5 degree
+            # rot_mat = pc_util.rotz(rot_angle)
+            # point_cloud[:, 0:3] = np.dot(point_cloud[:, 0:3], np.transpose(rot_mat))
+            # target_bboxes = rotate_aligned_boxes(target_bboxes, rot_mat)
 
-            if np.random.random() > 0.5:
-                # Flipping along the XZ plane
-                point_cloud[:, 1] = -1 * point_cloud[:, 1]
-                target_bboxes[:, 1] = -1 * target_bboxes[:, 1]
 
-                # Rotation along up-axis/Z-axis
-            rot_angle = (np.random.random() * np.pi / 18) - np.pi / 36  # -5 ~ +5 degree
-            rot_mat = pc_util.rotz(rot_angle)
-            point_cloud[:, 0:3] = np.dot(point_cloud[:, 0:3], np.transpose(rot_mat))
-            target_bboxes = rotate_aligned_boxes(target_bboxes, rot_mat)
+        # ------------------------------- LABELS ------------------------------
+        box3d_centers = np.zeros((MAX_NUM_OBJ, 3))
+        box3d_sizes = np.zeros((MAX_NUM_OBJ, 3))
+        angle_classes = np.zeros((MAX_NUM_OBJ,))
+        angle_residuals = np.zeros((MAX_NUM_OBJ,))
+        size_classes = np.zeros((MAX_NUM_OBJ,))
+        size_residuals = np.zeros((MAX_NUM_OBJ, 3))
+        label_mask = np.zeros((MAX_NUM_OBJ))
+        label_mask[0:bboxes.shape[0]] = 1
+
+
 
         # compute votes *AFTER* augmentation
         # generate votes
@@ -103,39 +119,66 @@ class WaymoDetectionDataset(Dataset):
             # find all points belong to that instance
             ind = np.where(instance_labels == i_instance)[0]
             # find the semantic label
-            if semantic_labels[ind[0]] in DC.nyu40ids:
-                x = point_cloud[ind, :3]
-                center = 0.5 * (x.min(0) + x.max(0))
+            if semantic_labels[ind[0]] in set(DC.type2class.values()):
+                x = point_cloud[ind,:3]
+                center = 0.5*(x.min(0) + x.max(0))
                 point_votes[ind, :] = center - x
                 point_votes_mask[ind] = 1.0
-        point_votes = np.tile(point_votes, (1, 3))  # make 3 votes identical
+        point_votes = np.tile(point_votes, (1, 3)) # make 3 votes identical
 
-        class_ind = [np.where(DC.nyu40ids == x)[0][0] for x in instance_bboxes[:, -1]]
-        # NOTE: set size class as semantic class. Consider use size2class.
-        size_classes[0:instance_bboxes.shape[0]] = class_ind
-        size_residuals[0:instance_bboxes.shape[0], :] = \
-            target_bboxes[0:instance_bboxes.shape[0], 3:6] - DC.mean_size_arr[class_ind, :]
+        for i in range(bboxes.shape[0]):
+            bbox = bboxes[i]
+            semantic_class = bbox[7]
+            box3d_center = bbox[0:3]
+            angle_class, angle_residual = DC.angle2class(bbox[6])
+            box3d_size = bbox[3:6]
+            size_class, size_residual = DC.size2class(box3d_size, DC.class2type[semantic_class])
+            box3d_centers[i,:] = box3d_center
+            angle_classes[i] = angle_class
+            angle_residuals[i] = angle_residual
+            size_classes[i] = size_class
+            size_residuals[i] = size_residual
+            box3d_sizes[i,:] = box3d_size
+
+        target_bboxes_mask = label_mask
+        target_bboxes = np.zeros((MAX_NUM_OBJ, 6))
+        for i in range(bboxes.shape[0]):
+            bbox = bboxes[i]
+            corners_3d = sunrgbd_utils.my_compute_box_3d(bbox[0:3], bbox[3:6], bbox[6])
+            # compute axis aligned box
+            xmin = np.min(corners_3d[:,0])
+            ymin = np.min(corners_3d[:,1])
+            zmin = np.min(corners_3d[:,2])
+            xmax = np.max(corners_3d[:,0])
+            ymax = np.max(corners_3d[:,1])
+            zmax = np.max(corners_3d[:,2])
+            target_bbox = np.array([(xmin+xmax)/2, (ymin+ymax)/2, (zmin+zmax)/2, xmax-xmin, ymax-ymin, zmax-zmin])
+            target_bboxes[i,:] = target_bbox
+
+        point_cloud, choices = pc_util.random_sampling(point_cloud, self.num_points, return_choices=True)
+        point_votes = point_votes[choices]
+        point_votes_mask = point_votes_mask[choices]
+        #point_votes_mask = point_votes[choices,0]
+        #point_votes = point_votes[choices,1:]
 
         ret_dict = {}
         ret_dict['point_clouds'] = point_cloud.astype(np.float32)
-        ret_dict['center_label'] = target_bboxes.astype(np.float32)[:, 0:3]
+        ret_dict['center_label'] = target_bboxes.astype(np.float32)[:,0:3]
         ret_dict['heading_class_label'] = angle_classes.astype(np.int64)
         ret_dict['heading_residual_label'] = angle_residuals.astype(np.float32)
         ret_dict['size_class_label'] = size_classes.astype(np.int64)
         ret_dict['size_residual_label'] = size_residuals.astype(np.float32)
         target_bboxes_semcls = np.zeros((MAX_NUM_OBJ))
-        target_bboxes_semcls[0:instance_bboxes.shape[0]] = \
-            [DC.nyu40id2class[x] for x in instance_bboxes[:, -1][0:instance_bboxes.shape[0]]]
+        target_bboxes_semcls[0:bboxes.shape[0]] = bboxes[:,-1] # from 0 to 9
         ret_dict['sem_cls_label'] = target_bboxes_semcls.astype(np.int64)
         ret_dict['box_label_mask'] = target_bboxes_mask.astype(np.float32)
         ret_dict['vote_label'] = point_votes.astype(np.float32)
         ret_dict['vote_label_mask'] = point_votes_mask.astype(np.int64)
         ret_dict['scan_idx'] = np.array(idx).astype(np.int64)
-        ret_dict['pcl_color'] = pcl_color
         return ret_dict
 
 if __name__ == '__main__':
     dset = WaymoDetectionDataset(data_path=sys.argv[1])
-    for i in range(4):
+    for i in range(10):
         example = dset.__getitem__(i)
-        print(example)
+        print(example['vote_label'].shape)
